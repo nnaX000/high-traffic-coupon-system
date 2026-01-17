19ab7a31be54fde8651549518eae8ad8a5c1ff57
김나영 <kny200208@naver.com>
2026-01-17 17:18:01 +0900

    fix : jwtToken 토큰들 toString에 노출x

--- src/main/java/com/example/coupon/dto/JwtToken.java
+++ src/main/java/com/example/coupon/dto/JwtToken.java
@@ -3,12 +3,17 @@ package com.example.coupon.dto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.ToString;
 
 @Builder
 @AllArgsConstructor
 @Data
 public class JwtToken {
     private String grantType; // Bearer 인증 방식
+
+    @ToString.Exclude
     private String accessToken;
+
+    @ToString.Exclude
     private String refreshToken;
 }
