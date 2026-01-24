193a0c333e70ae9e7acc9eeb4c8cf392aa93ef0d
김나영 <kny200208@naver.com>
2026-01-24 22:19:54 +0900

    feat : UserRepository

---
+++ src/main/java/com/example/coupon/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.example.coupon.repository;
+
+import com.example.coupon.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByUserId(String userId);
+    Optional<User> findByUserId(String userId);
+    Optional<User> findByUsername(String userId);
+}
