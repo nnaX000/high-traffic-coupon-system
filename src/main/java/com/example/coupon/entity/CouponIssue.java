4f26d8645c41ca7ea6ad231a86b86d187baac2d1
김나영 <kny200208@naver.com>
2026-01-30 21:03:42 +0900

    feat : Create Couponissue entity

---
+++ src/main/java/com/example/coupon/entity/CouponIssue.java
@@ -0,0 +1,46 @@
+package com.example.coupon.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(
+        name = "coupon_issue",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = {"user_id", "coupon_id"})
+        }
+)
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CouponIssue {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // 발급 받은 유저
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    // 발급된 쿠폰
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "coupon_id", nullable = false)
+    private Coupon coupon;
+
+    // 발급 시간
+    @Column(nullable = false)
+    private LocalDateTime issuedAt;
+
+    // 사용 여부
+    @Column(nullable = false)
+    private boolean used = false;
+
+    public void use() {
+        this.used = true;
+    }
+}
