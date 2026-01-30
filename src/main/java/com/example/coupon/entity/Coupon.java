52f64aeffe9d972af1838f0e6e0f98ad0d078bfa
김나영 <kny200208@naver.com>
2026-01-30 21:19:13 +0900

    fix : modify @id dependency

--- src/main/java/com/example/coupon/entity/Coupon.java
+++ src/main/java/com/example/coupon/entity/Coupon.java
@@ -4,7 +4,6 @@ import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
 
 @Entity
 @Table(name = "coupon")
