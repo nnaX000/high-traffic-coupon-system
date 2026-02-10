d75451511ff9ea6726db98a58e3cab1e1ad2a9dc
김나영 <kny200208@naver.com>
2026-02-10 23:49:51 +0900

    fix : 이중슬래시 제거

--- src/main/java/com/example/coupon/controller/CouponController.java
+++ src/main/java/com/example/coupon/controller/CouponController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("api/coupons/")
+@RequestMapping("/api/coupons")
 public class CouponController {
 
     private final CouponService couponService;
