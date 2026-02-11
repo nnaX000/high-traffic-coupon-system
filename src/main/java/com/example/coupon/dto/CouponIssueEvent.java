00d08d63504a871d3228edcc043f8513d527ecb5
김나영 <kny200208@naver.com>
2026-02-11 23:22:45 +0900

    fix: add no-args constructor to Kafka DTO for deserialization

--- src/main/java/com/example/coupon/dto/CouponIssueEvent.java
+++ src/main/java/com/example/coupon/dto/CouponIssueEvent.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class CouponIssueEvent {
     private Long couponId;
     private String username;
